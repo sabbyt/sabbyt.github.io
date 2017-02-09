4c17d51c81edfc74c01e4c2cf0c533af3cce1b2a
Sabrina <sabrina@redapesolutions.com>
2017-02-09 21:20:34 +0800

    Shifted files around

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $( "#sendEmail" ).submit(function() {
   const emailDetails =
-    'mailto:' + $( "#email" ).val() +
+    'mailto:sabrinatee@me.com' +
     '?subject=Inquiry from: ' + $( "#first_name" ).val() + ' ' + $( "#last_name" ).val() +
-    '&body=' + $( "#message" ).val()
+    '&body=' + $( "#message" ).val() + 'Sent from: ' + $( "#email" ).val();
   window.open(emailDetails);
 });
